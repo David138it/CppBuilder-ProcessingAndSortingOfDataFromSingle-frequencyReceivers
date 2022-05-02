3e72acb91cb86605671d20a4ddfe76d25151d30c
David138it <david138it@gmail.com>
2022-05-02 22:40:51 +0800

    Step5 CppBuilder-ProcessingAndSortingOfDataFromSingle-frequencyReceiversSRNS1

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 <p><strong>Task</strong>:<br />
 Create a program in C++ that allows the data received from the receiver from RINEX files in text format to obtain the pseudodality difference (this is the distance between the satellite and the receiver obtained by multiplying the value of the speed of light with the time delay in signal propagation measured by the satellite and receiver clocks) of the satellite and sorts the pseudodalities for each navigation to the satellite to simplify further processing. Here's what the file looks like irkj1380.txt , in which you need to sort the pseudodality for each navigation satellite. We took this data from the base station.<br />
-we need to make sure that the string G01G04G11G12G14G18G22G24G25G31G32R05 is read by letters, that is, we create a dynamic array with a pointer
-</p>
+<strong>Task</strong>:<br />
+Let's add a condition so that the names of satellites with numbers can be read in full</p>
 <p><strong>Manual</strong>:</p>
 <ol>
 <li>The IDE for C/C++ development - Borland Builder must be installed. I recommend installing the IDE in Windows using the following link: <a href="https://www.youtube.com/watch?v=bDvVosvyVp0&list=LL&index=315" target="_blank">youtube</a></li>
@@ -12,4 +12,4 @@ we need to make sure that the string G01G04G11G12G14G18G22G24G25G31G32R05 is rea
 <p><strong>Tools</strong>:</p>
 <ol>
 <li>Bornald Builder</li>
-</ol>
+<ol>
--- Unit1.cpp
+++ Unit1.cpp
@@ -45,8 +45,24 @@ __fastcall TForm1::TForm1(TComponent* Owner)
         int countSputnik;
         in >> countSputnik; // reading the number of satellites - 19
         out<< countSputnik << endl; // output the number of satellites
-        // output the names of the satellites in the line:
+        // 4 output the names of the satellites in the line:
         getline(in,name);// we read the remaining line with the names of the satellites
-        out<< name;
+        out<< name << endl;
+        // 5 we need to make sure that the string G01G04G11G12G14G18G22G24G25G31G32R05 is read by letters, that is, we create a dynamic array with a pointer: 
+        string *Sputnik = new string [countSputnik];
+        // this is a loop that runs through the name variable, in which you need to pull out the names of the satellites:
+        for(int i=0; i<countSputnik; i++){
+                Sputnik[i] = "";
+        }
+        //reading the first line:
+        int index=-1;
+        for(int i=0; i< name.size(); i++) {
+                if(name[i] == '-')
+                        break;// If there is a "-" in the line with the names of the satellites, then abort the operation
+                if(name[i] >= 'A' && name[i] <= 'Z') {
+                        index++;
+                        out << name[i] << endl;//this condition gives out only the letters of the satellites
+                }
+        }
 }
 //---------------------------------------------------------------------------
