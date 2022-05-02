c746525f1c695a6c62af23427e429120af7ceaeb
David138it <david138it@gmail.com>
2022-05-02 22:20:28 +0800

    Step2 CppBuilder-ProcessingAndSortingOfDataFromSingle-frequencyReceiversSRNS1

--- Unit1.cpp
+++ Unit1.cpp
@@ -18,16 +18,19 @@ __fastcall TForm1::TForm1(TComponent* Owner)
 {
         ifstream in ("irkj1380.txt");
         ofstream out ("out.txt");
-        //Let's try in the file irkj1380.txt output all information up to the END OF HEADER in out.txt:
+        // Let's try in the file irkj1380.txt output all information up to the END OF HEADER in out.txt:
         string a;
         while(true) {
                 getline (in,a); //reading the first line in the file
                 out<<a<<endl;
-                //we give a condition that when the program sees a string with the value END OF HEADER, the program is interrupted:
+                // we give a condition that when the program sees a string with the value END OF HEADER, the program is interrupted:
                 if(a == "                                                            END OF HEADER") {
                         break;
                 }
-        }  
+        } 
+        // Print the following line after END OF HEADER: 
+        string name;
+        getline(in,name);// read the following line
+        out<<name<<endl;// output this line
 }
 //---------------------------------------------------------------------------
- 
